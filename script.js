9ae734520adc23ecad9717bf352a550832727163
violacathrine <cathrineohlsson@live.se>
2025-02-19 19:53:13 +0100

    changes in script-file, menu close when click outside

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+
     const menuToggle = document.getElementById("menu-toggle");
     const navbar = document.getElementById("navbar");
 
-    menuToggle.addEventListener("click", function () {
+    menuToggle.addEventListener("click", function (event) {
         navbar.classList.toggle("active");
         menuToggle.classList.toggle("active");
+        event.stopPropagation();
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!navbar.contains(event.target) && !menuToggle.contains(event.target)) {
+            navbar.classList.remove("active");
+            menuToggle.classList.remove("active");
+        }
     });
 
     document.querySelectorAll(".navbar a").forEach(link => {
