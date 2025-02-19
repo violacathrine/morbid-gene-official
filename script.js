8a6595f2571fcf109bb290730e08625e494ddc02
violacathrine <cathrineohlsson@live.se>
2025-02-19 19:29:30 +0100

    padding to webdesign and policy

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
         menuToggle.classList.toggle("active");
     });
 
-    // Stäng menyn när en länk klickas
     document.querySelectorAll(".navbar a").forEach(link => {
         link.addEventListener("click", function () {
             navbar.classList.remove("active");
